a9371768b4e21d2ae9a2bad7597f0aed6072977b
Dale Terrell <tragicid@gmail.com>
2019-09-06 18:43:45 +0000

    done

--- lib/ruby_1.rb
+++ lib/ruby_1.rb
@@ -14,4 +14,33 @@ module Ruby1
   puts "I've got a random number between 1 and 100"
   puts "Can you guess it?"
   target = rand(100) + 1
+  
+  #track number of guesses
+  num_guesses = 0
+  
+  #track whether the player has guessed correctly
+  guessed_it = false 
+  
+  until num_guesses == 10 || guessed_it
+  
+  puts "You've got #{10 - num_guesses} guesses left."
+  print "Make a guess: "
+  guess = gets.to_i 
+  
+  num_guesses += 1 
+  
+  #compare the guess to the target
+  #print the appropriate message
+  if guess < target 
+    puts "Oops.  Your guess was too LOW."
+  if guess > target
+    puts "Dangit.  Your guess was too HIGH."
+  elsif guess == target
+    puts "Good job, #{name}!"
+    puts "You guessed my number in #{num_guesses} guesses!"
+    guessed_it = true 
+  end 
+end 
+  
+    
 end
